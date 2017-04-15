7737f4403c7b953588aaea26476a245c556e9f41
Robert JH Dukes <robert.dukes@outlook.com>
2017-04-15 01:57:09 +0100

    Update fakenews.js

--- Extension/fakenews.js
+++ Extension/fakenews.js
@@ -47,4 +47,9 @@
 })($);
 
 document.addEventListener('DOMContentLoaded', function() {
- );
+    var link = document.getElementById('link');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+        hellYeah('xxx');
+    });
+});
