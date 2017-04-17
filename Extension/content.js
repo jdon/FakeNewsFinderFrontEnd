8dfd7a93c90c5216db668055cfb837c612301ede
Robert JH Dukes <robert.dukes@outlook.com>
2017-04-17 16:59:37 +0100

    Delete content.js

--- Extension/content.js
+++
@@ -1,7 +0,0 @@
-var scripts = ["fakenews.js"];
-for (var i = 0; i < scripts.length; i++) {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = chrome.extension.getURL(scripts[i]);
-  document.getElementsByTagName("body")[0].appendChild(script)
-}
