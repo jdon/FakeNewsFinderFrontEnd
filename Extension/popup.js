5c663182c870a497584cb86aa2305c6caca3e209
L4wrence <lawrencemathurin@googlemail.com>
2017-04-19 20:38:31 +0100

    reverted back to showing all links
    Didn't always show the links if ran multiple times

--- Extension/popup.js
+++ Extension/popup.js
@@ -2,7 +2,8 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {type: "getContent"}, 
 
     function(response) {
-		document.getElementById("memes").innerHTML += "List of fake articles on current page";
+    	console.log(response);
+		document.getElementById("memes").innerHTML += "Here are all the links we found on the page, you can find out if they are fake or not by clicking them:";
 			for (let i = 0; i < response.length; i++) {
 
 				//returns the JSON file from backend
@@ -11,7 +12,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 				
 
 				$.getJSON(jsonpath, function(data){
-					if (data.domainList.fake) {
+					if (data.title) {
 						var link = "http://fake.shanney.co.uk/index.php/" + response[i]
 						document.getElementById("output").innerHTML += "<p id='" + i + "'><a href='" + link + "' target='_blank'>" + data.title + "</a></p>";
 					}
