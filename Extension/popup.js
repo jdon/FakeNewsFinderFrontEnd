4cb9d50278dba24cd716462f92c2684504eb1184
L4wrence <lawrencemathurin@googlemail.com>
2017-04-19 16:37:42 +0100

    Only shows links that are fake

--- Extension/popup.js
+++ Extension/popup.js
@@ -2,8 +2,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {type: "getContent"}, 
 
     function(response) {
-		document.getElementById("memes").innerHTML += "List of Found Links on current page";
-        console.log(response);
+		document.getElementById("memes").innerHTML += "List of fake articles on current page";
 			for (let i = 0; i < response.length; i++) {
 
 				//returns the JSON file from backend
@@ -12,7 +11,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 				
 
 				$.getJSON(jsonpath, function(data){
-					if (data.title) {
+					if (data.domainList.fake) {
 						var link = "http://fake.shanney.co.uk/index.php/" + response[i]
 						document.getElementById("output").innerHTML += "<p id='" + i + "'><a href='" + link + "' target='_blank'>" + data.title + "</a></p>";
 					}
