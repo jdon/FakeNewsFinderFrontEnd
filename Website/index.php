e9d59bbd02d3dfd1090f5956121661ecefed5a90
L4wrence <lawrencemathurin@googlemail.com>
2017-04-24 16:28:20 +0100

    Fixed bug, changed request
    Removed CORS and changed added a GET request for the URL. Fixed the bug where the page wouldn't load the report if a link on the extension was clicked

--- Website/index.php
+++ Website/index.php
@@ -14,73 +14,73 @@
 
 <!--///////////////////////////////////////-->
 
-<script> $(window).load(function() {
+<script>
+$(window).load(function(){
     search();
 });
 
-function createCORSRequest(method, url) {
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr) {
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined") {
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        xhr = null;
-    }
-    return xhr;
+function clear(){
+    //Clears page before loading new results
+    document.getElementById("domain").innerHTML = "";
+    document.getElementById("title").innerHTML = "";
+    document.getElementById("author").innerHTML = "";
+    document.getElementById("fake").innerHTML = "";
+    document.getElementById("type").innerHTML = "";
+    document.getElementById("type1").innerHTML = "";
+    document.getElementById("type2").innerHTML = "";
+    document.getElementById("type3").innerHTML = "";    
 }
 
-function search() {
-    var xhr = createCORSRequest('GET', "http://fake.jdon.uk/url/" + encodeURIComponent($("#searchbox").val()));
-    if (!xhr) {
-        throw new Error('CORS not supported');
-    }
-    xhr.onload = function(e) {
-        if (this.status == 200) {
-            var json = this.response;
-        }
-        $.getJSON("tag-descriptions.json", function(data) {
-            console.log(data);
-            var obj = JSON.parse(json);
-            console.log(obj);    
-            if (!(obj.domain)) {
-                document.getElementById("domain").innerHTML = "";
-            } else {
-                document.getElementById("domain").innerHTML = "<span class='DomainID'>Wesbite: </span> <br>" + obj.domain;
+function search(){
+    //http://weeklyworldnews.com/headlines/49503/leprechauns-attack/
+    //http://www.bbc.co.uk/news/uk-39678863
+    clear();
+    //Retrieve JSON from backend
+    var url = document.getElementById("searchbox").value;
+    var uri = encodeURIComponent(url);
+    var jsonpath = ("http://fake.jdon.uk/url/" + uri);
+    var jsonpath2 = "http://fakenewsfinder.azurewebsites.net/tag-descriptions.json"
+
+    $.getJSON(jsonpath, function(data){
+
+        console.log(data);
+        $.getJSON(jsonpath2, function(tags){
+
+            console.log(tags);
+
+            if (data.domain) {
+                document.getElementById("domain").innerHTML = "<span class='DomainID'>Wesbite: </span> <br>" + data.domain;
             }
-            if (!(obj.title)) {
+             if (!(data.title)) {
                 document.getElementById("title").innerHTML = "Sorry, the title of the article is not avaliable";
             } else {
-                document.getElementById("title").innerHTML = "<span class='ArticleID'>Article Title: </span> <br>" + obj.title;
-            };
-            if (!(obj.author)) {
-                document.getElementById("author").innerHTML = "";
-            } else {
-                document.getElementById("author").innerHTML = "<span class='AuthorID'>Author: </span> <br>" + obj.author;
+                document.getElementById("title").innerHTML = "<span class='ArticleID'>Article Title: </span> <br>" + data.title;
             }
-            if (obj.domainList.fake) {
+            if (data.author) {
+                document.getElementById("author").innerHTML = "<span class='AuthorID'>Author: </span> <br>" + data.author;
+            } 
+
+            if (data.domainList.fake) {
                 document.getElementById("fake").innerHTML = " <span class='FakeID'>FAKE</span> <br> Unfortunately, this article has been flagged based on our checks. <br> Check out our reasoning below. ";
 
                 document.getElementById("type").innerHTML = "<span class='AuthorID'>Reasons: </span>";
-                if (obj.domainList.notes.type1) {
-                    document.getElementById("type1").innerHTML =  data.tags[obj.domainList.notes.type1].description;
+                if (data.domainList.notes.type1) {
+                    document.getElementById("type1").innerHTML =  tags.tags[data.domainList.notes.type1].description;
                 }
-                if (obj.domainList.notes.type2) {
-                    document.getElementById("type2").innerHTML = data.tags[obj.domainList.notes.type2].description;
+                if (data.domainList.notes.type2) {
+                    document.getElementById("type2").innerHTML = tags.tags[data.domainList.notes.type2].description;
                 }
-                if (obj.domainList.notes.type3) {
-                    document.getElementById("type3").innerHTML = data.tags[obj.domainList.notes.type3].description;
+                if (data.domainList.notes.type3) {
+                    document.getElementById("type3").innerHTML = tags.tags[data.domainList.notes.type3].description;
                 }
             } else {
                 document.getElementById("fake").innerHTML = " <span class='FakeID'>NOT FAKE<br> </span>This article has been deemed not fake based on our checks.";
-                	document.getElementById("reasons").innerHTML = "";
-					document.getElementById("type").innerHTML = "";
             }
         });
-    };
-    xhr.send();
-} </script>
+    });
+}
+
+</script>
 
 <!--///////////////////////////////////////////////-->
 	</head>
@@ -103,7 +103,7 @@ function search() {
 
 			<div id="check">
 					<div class="search">
-	<input id="searchbox" value="<?php echo ltrim($_SERVER['PATH_INFO'], '/https:/'); ?>" type="text">
+	<input id="searchbox" value="<?php echo ltrim($_GET["url"], '/https:/') ?>" type="text">
 			<button class="button" onload="search()" onclick="search()">►</button>
 			
 			<div id="newbox">
